b4727d8b6f077add4bd93a3014e8319f9daf1ddc
xabier_laibarra <xlaibarra@gmail.com>
2026-09-13 11:22:22 +0200

    modify stg_customers staging view

--- models/staging/stg_costumers.sql
+++ models/staging/stg_costumers.sql
@@ -1,1 +1,11 @@
-SELECT * FROM {{source('staging', 'customers') }}
+
+WITH source_data AS (
+	SELECT customer_id,
+	       first_name,
+	       last_name,
+	       email
+	FROM {{source('staging', 'customers') }}
+)
+
+
+SELECT * FROM source_data
